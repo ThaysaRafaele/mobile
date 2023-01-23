455280189fb66ceb582bfbb107014d7c252d849b
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-01-23 00:13:39 -0400

    finalização mobile

---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { View } from "react-native";
+
+import { AppRoutes } from "./app.routes";
+import React from "react";
+
+export function Routes() {
+  return (
+    <View className="flex-1 bg-background">
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
+  )
+}
