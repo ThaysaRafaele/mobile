2d1afc750f42e666eead61fdc746ab04c284552d
Thaysa Rafaele Gomes Lacerda <thaysa.lacerda@byseven.com.br>
2023-01-23 00:13:59 -0400

    finalização mobile

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import{
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
+import { 
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_800ExtraBold
-} from '@expo-google-fonts/inter'
-import React from 'react';
+} from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,29 +20,16 @@ export default function App() {
     Inter_800ExtraBold
   });
 
-  if(!fontsLoaded){
+  if (!fontsLoaded) {
     return (
-      <Loading/>
-    )
+      <Loading />
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Testando a renderização!</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    <>
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color:'#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
